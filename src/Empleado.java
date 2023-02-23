6e883cc0f3712fb55569e6ee4f1ef72132f28a35
Vladimir Torres <vlacky810@gmail.com>
2023-02-23 15:57:24 -0500

    primer commit

---
+++ src/Empleado.java
@@ -0,0 +1,37 @@
+public class Empleado {
+
+    private String nombre;
+    private String identificacion;
+    private double salario;
+
+    public Empleado() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getBonificacion(){
+        return this.salario * 0.1;
+    }
+}
---
+++ src/TestEmpleado.java
@@ -0,0 +1,12 @@
+public class TestEmpleado {
+
+    public static void main(String[] args) {
+        Empleado diego = new Empleado();
+        diego.setNombre("Diego");
+        diego.setIdentificacion("123456");
+        diego.setSalario(1000.0);
+
+        System.out.println(diego.getSalario());
+        System.out.println(diego.getBonificacion());
+    }
+}
